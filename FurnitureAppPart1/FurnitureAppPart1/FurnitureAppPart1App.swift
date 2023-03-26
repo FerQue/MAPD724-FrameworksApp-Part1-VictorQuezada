b22a752984d1d51a10d6e17e152d18ffb51a28e0
FerQue <fernando_quezada1785@outlook.com>
2023-03-26 14:55:37 -0400

    Update_1

---
+++ FurnitureAppPart1/FurnitureAppPart1/FurnitureAppPart1App.swift
@@ -0,0 +1,17 @@
+//
+//  FurnitureAppPart1App.swift
+//  FurnitureAppPart1
+//
+//  Created by Victor Quezada on 2023-03-25.
+//
+
+import SwiftUI
+
+@main
+struct FurnitureAppPart1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
